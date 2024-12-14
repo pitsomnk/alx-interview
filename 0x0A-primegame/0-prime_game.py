bbc20ddc1698dda7f72cfb27013ce9d4e495ddc7
Pitso manyike <pitsomnk@gmail.com>
2024-12-14 11:54:38 -0800

    anotherone

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,11 +9,11 @@ def isWinner(x, nums):
     Args:
         x (int): The number of rounds.
         nums (list of int): A list of integers where each integer n denotes
-        a set of consecutive integers starting from 1 up to and including n.
+            a set of consecutive integers starting from 1 up to and including n.
 
     Returns:
         str: The name of the player who won the most rounds (either "Ben"
-        or "Maria").
+            or "Maria").
         None: If the winner cannot be determined.
 
     Raises:
@@ -24,18 +24,23 @@ def isWinner(x, nums):
         return None
     if x != len(nums):
         return None
+    
     # Initialize scores and array of possible prime numbers
     ben = 0
     maria = 0
+    
     # Create a list 'a' of length sorted(nums)[-1] + 1 with all elements
     # initialized to 1
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a = [1 for _ in range(sorted(nums)[-1] + 1)]
+    
     # The first two elements of the list, a[0] and a[1], are set to 0
     # because 0 and 1 are not prime numbers
     a[0], a[1] = 0, 0
+    
     # Use Sieve of Eratosthenes algorithm to generate array of prime numbers
     for i in range(2, len(a)):
         rm_multiples(a, i)
+    
     # Play each round of the game
     for i in nums:
         # If the sum of prime numbers in the set is even, Ben wins
@@ -43,6 +48,7 @@ def isWinner(x, nums):
             ben += 1
         else:
             maria += 1
+    
     # Determine the winner of the game
     if ben > maria:
         return "Ben"
@@ -77,4 +83,5 @@ def rm_multiples(ls, x):
             ls[i * x] = 0
         except (ValueError, IndexError):
             break
+
 
