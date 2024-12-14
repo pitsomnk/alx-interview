3d6c89324169cb5121462ed53f653e38d6f32239
Pitso manyike <pitsomnk@gmail.com>
2024-12-14 11:37:38 -0800

    primeIsfunny

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -55,3 +55,4 @@ def primes_in_range(start, end):
     """Returns a list of prime numbers between start and end (inclusive)."""
     primes = [n for n in range(start, end+1) if is_prime(n)]
     return primes
+
